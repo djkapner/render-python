e837c7ae4eaa710b953e4d33975d835b588d9499
Dan Kapner <danielk@.alleninstitute.org>
2018-12-18 12:10:11 -0800

    40% time saving from russel with sqeuclidean

--- renderapi/transform/leaf/thin_plate_spline.py
+++ renderapi/transform/leaf/thin_plate_spline.py
@@ -97,15 +97,10 @@ class ThinPlateSplineTransform(Transform):
         return result
 
     def computeDeformationContribution(self, points):
-        #disp = scipy.spatial.distance.cdist(
-        #        points,
-        #        self.srcPts.transpose())
-        #disp = np.power(disp, 2.0) * np.ma.log(disp).filled(0.0)
         disp = scipy.spatial.distance.cdist(
                 points,
                 self.srcPts.transpose(),
                 metric='sqeuclidean')
-        #disp[disp != 0.] *= np.log(np.sqrt(disp[disp != 0.]))
         disp *= np.ma.log(np.sqrt(disp)).filled(0.0)
         return disp.dot(self.dMtxDat.transpose())
 
